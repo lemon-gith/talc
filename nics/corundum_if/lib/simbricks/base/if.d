2c6fb121169165770cc30fcf7d32a7085d389149
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2025-05-12 19:29:33 +0100

    ignore object and other processing files

--- nics/corundum_if/lib/simbricks/base/if.d
+++
@@ -1,4 +0,0 @@
-lib/simbricks/base/if.o: lib/simbricks/base/if.c lib/simbricks/base/if.h \
- lib/simbricks/base/proto.h
-lib/simbricks/base/if.h:
-lib/simbricks/base/proto.h:
--- nics/corundum_if/lib/simbricks/pcie/if.d
+++
@@ -1,8 +0,0 @@
-lib/simbricks/pcie/if.o: lib/simbricks/pcie/if.c lib/simbricks/pcie/if.h \
- lib/simbricks/base/generic.h lib/simbricks/base/if.h \
- lib/simbricks/base/proto.h lib/simbricks/pcie/proto.h
-lib/simbricks/pcie/if.h:
-lib/simbricks/base/generic.h:
-lib/simbricks/base/if.h:
-lib/simbricks/base/proto.h:
-lib/simbricks/pcie/proto.h:

6b380d1bd37bea97c3518088a81633f3d7d33590
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2025-06-03 20:09:38 +0100

    configure script to be more callable

--- src/tap/py/scripts/tap-config.sh
+++ src/tap/py/scripts/tap-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xe
+devname=${1:-tap0}
 
-sudo ip link set tap0 up
-sudo ip addr add 10.0.0.1/24 dev tap0
+sudo ip link set $devname up
+sudo ip addr add 10.0.0.1/24 dev $devname

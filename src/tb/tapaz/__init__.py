c6693f3b7cfae140283f3281445e5e8ca7a7c6b2
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2025-06-05 13:58:45 +0100

    reexport tapaz modules for easier import

--- src/tb/tapaz/__init__.py
+++ src/tb/tapaz/__init__.py
@@ -4,3 +4,8 @@ Puts TAP interfaces wrapped by netlib to use
 Designed to work with the Corundum testing framework, hence the name:
 it's a play on topaz, another mineral, like Corundum
 """
+
+#ruff: noqa F401
+
+from tapaz.client import TAPClient
+from tapaz.server import TAPServer
